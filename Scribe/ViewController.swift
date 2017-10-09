9dae91fe210b904e57c020dadc28d695387bf340
exxe <exxe06@marcterencesmbp.fritz.box>
2017-10-09 15:31:03 +0200

    Initial Commit

---
+++ Scribe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scribe
+//
+//  Created by exxe on 09/10/2017.
+//  Copyright © 2017 exxe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
